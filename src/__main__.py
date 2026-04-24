a0cbe9cf156f5f12e1b9452c0a3bcf9480088e77
mochine-tb <mouhsinv1234@gmail.com>
2026-04-24 13:57:29 +0100

    inital file structure

---
+++ src/__main__.py
@@ -0,0 +1,16 @@
+def main():
+    # -- parsing --
+    # load functions definitions into model objects
+    #
+    # load prompts into model objects
+    #
+    # -- generation pipeline --
+    #
+    # -- ouput --
+    # format and save ouput
+    #
+    print("Hello from call-me-maybe-retry!")
+
+
+if __name__ == "__main__":
+    main()
