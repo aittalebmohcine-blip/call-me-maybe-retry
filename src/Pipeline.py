17cee5fa87051dfb47fb127ff9e3c862859af0b5
Mohcine Ait Taleb <mait-tal@e1r13p4.1337.ma>
2026-04-30 13:08:57 +0100

    feature: build tree

--- src/Pipeline.py
+++ src/Pipeline.py
@@ -1,19 +1,62 @@
 from pydantic import BaseModel, Field
-from typing import Dict, Set, Any
+from typing import Dict
+import json
 
 from llm_sdk import Small_LLM_Model
 
 NEGATIVE_INF = float('-inf')
-
-# Trie node: maps token_id → child node, plus is_terminal flag
-# {"children": {token_id: TrieNode}, "terminal": bool}
-TrieNode = dict
-
 model: Small_LLM_Model = Small_LLM_Model()
-NUM_TOKEN_IDS = set(model.encode("0123456789.-+eE\n")[0].tolist())
-FORBIDDEN_STR_TOKEN_IDS = {364, 330}
+
+# ------------------------
+# buudling the trie for fast id look up
+# ------------------------
+path = model.get_path_to_vocab_file()
+
+with open(path, "r", encoding="utf-8") as f:
+    vocab = json.load(f)
 
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.token_ids = []
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, token_text, token_id):
+        node = self.root
+
+        for ch in token_text:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+
+            node = node.children[ch]
+            node.token_ids.append(token_id)
+
+    def lookup(self, prefix):
+        node = self.root
+
+        for ch in prefix:
+            if ch not in node.children:
+                return []
+
+            node = node.children[ch]
+
+        return node.token_ids
+
+
+trie = Trie()
+
+for token_text, token_id in vocab.items():
+    trie.insert(token_text, token_id)
+
+
+# --------------------------
+# pipline
+# --------------------------
 class Pipeline(BaseModel):
     functions_by_name: Dict = Field(...,
                                     description="List of function definitions")
