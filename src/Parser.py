72bb4b462cc46f0ca35bbcd535d88ba88b1ef05d
Mohcine Ait Taleb <mait-tal@e2r10p15.1337.ma>
2026-05-08 13:15:48 +0100

    feature: default input and ouput files

--- src/Parser.py
+++ src/Parser.py
@@ -1,4 +1,5 @@
-from typing import List
+from pathlib import Path
+from typing import List, Dict
 import json
 
 from pydantic import BaseModel, Field
@@ -9,14 +10,31 @@ from src.Models import FunctionDefinition
 class Parser(BaseModel):
     func_defs_path: str = Field(
         default="data/input/functions_definition.json")
+
     prompts_path: str = Field(default="data/input/function_calling_tests.json")
+
+    path_to_ouput_file: str = Field(
+        default="data/output/function_calling_result.json")
 
     def parse_func_defs(self) -> List["FunctionDefinition"]:
         functions: List["FunctionDefinition"] = []
 
         # load data from the file
-        with open(self.func_defs_path) as f:
-            data = json.load(f)
+        try:
+            with open(self.func_defs_path) as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            raise ValueError(
+                f"ERROR: could not find the input file '{self.func_defs_path}'"
+            )
+        except PermissionError:
+            raise ValueError(
+                "ERROR: you don't have permission"
+                f" to input file '{self.func_defs_path}'"
+            )
+
+        except IsADirectoryError:
+            raise ValueError(f"'{self.func_defs_path}' is a directory !")
 
         # extract function definitions objs
         for element in data:
@@ -28,11 +46,42 @@ class Parser(BaseModel):
         prompts: List[str] = []
 
         # load data from the file
-        with open(self.prompts_path) as f:
-            data = json.load(f)
+        try:
+            with open(self.prompts_path) as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            raise ValueError(
+                f"ERROR: could not find the input file '{self.prompts_path}'"
+            )
+
+        except PermissionError:
+            raise ValueError(
+                "ERROR: you don't have permission"
+                f" to input file '{self.prompts_path}'"
+            )
+
+        except IsADirectoryError:
+            raise ValueError(f"'{self.prompts_path}' is a directory !")
 
         # extract prompts
         for element in data:
             prompts.append(element["prompt"])
 
         return prompts
+
+    def dump_output(self, ouput: List[Dict]) -> None:
+        try:
+            output_path = Path(self.path_to_ouput_file)
+            output_path.parent.mkdir(parents=True, exist_ok=True)
+
+            with open(output_path, "w") as f:
+                json.dump(ouput, f, indent=2)
+
+        except PermissionError:
+            raise ValueError(
+                "ERROR: you don't have permission"
+                f" to ouput file '{self.path_to_ouput_file}'"
+            )
+
+        except IsADirectoryError:
+            raise ValueError(f"'{self.path_to_ouput_file}' is a directory !")
